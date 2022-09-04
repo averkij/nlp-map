ef7dd9fa276523ee1153b2138f1ead24c25f7bf2
Sergei Averkiev <s.averkiev@kryptonite.ru>
2022-09-04 11:34:29 +0300

    Update map.

--- make_html.py
+++ make_html.py
@@ -4,10 +4,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
+import plotly.io as pio
+
+pio.templates.default = "seaborn"
 
 lang_df = pd.read_csv("./langs.csv")
 
 fig = go.Figure()
+fig.update_geos(projection_type="natural earth")
+fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
+fig.update_geos(
+    showcoastlines=True,
+    coastlinecolor="#aaaaaa",
+)
+
+fig.update_layout(
+    font_color="#555555",
+    title_font_color="#444444",
+    title_text="Language models on the map",
+    title_x=0.5,
+    title_font_size=20,
+    legend_x=0.92,
+)
+
 colors = px.colors.qualitative.Light24
 
 groups = {
@@ -25,6 +44,7 @@ for name, val in groups.items():
             text=lang_df[lang_df[val["col"]] == True]["Name"],
             mode="markers",
             marker_color=val["color"],
+            marker_symbol="diamond",
             visible=True,
         )
     )
@@ -33,6 +53,7 @@ fig.update_layout(
     autosize=True,
 )
 fig.update_layout(showlegend=True)
+
 
 fig.write_html("index.html", include_plotlyjs="cdn")
 
