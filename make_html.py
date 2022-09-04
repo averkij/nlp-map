6b3406954137cc3ff01d5c411c53bd9566f9aa33
Sergei Averkiev <s.averkiev@kryptonite.ru>
2022-09-04 11:00:39 +0300

    Update script and map.

--- make_html.py
+++ make_html.py
@@ -5,49 +5,29 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 
-lang_df = pd.read_csv("./langs_updated.csv")
+lang_df = pd.read_csv("./langs.csv")
 
 fig = go.Figure()
 colors = px.colors.qualitative.Light24
 
-# add mt5
-fig.add_trace(
-    go.Scattergeo(
-        name="mT5",
-        lon=lang_df[lang_df["mt5"] == True]["longitude"],
-        lat=lang_df[lang_df["mt5"] == True]["latitude"],
-        text=lang_df[lang_df["mt5"] == True]["Name"],
-        mode="markers",
-        marker_color="blue",
-        visible=True,
-    )
-)
+groups = {
+    "mT5": {"col": "mt5", "color": "blue"},
+    "mGPT": {"col": "mgpt", "color": "red"},
+    "BLOOM": {"col": "bloom", "color": "green"},
+}
 
-# add mgpt
-fig.add_trace(
-    go.Scattergeo(
-        name="mGPT",
-        lon=lang_df[lang_df["mgpt"] == True]["longitude"],
-        lat=lang_df[lang_df["mgpt"] == True]["latitude"],
-        text=lang_df[lang_df["mgpt"] == True]["Name"],
-        mode="markers",
-        marker_color="red",
-        visible=True,
+for name, val in groups.items():
+    fig.add_trace(
+        go.Scattergeo(
+            name=name,
+            lon=lang_df[lang_df[val["col"]] == True]["longitude"],
+            lat=lang_df[lang_df[val["col"]] == True]["latitude"],
+            text=lang_df[lang_df[val["col"]] == True]["Name"],
+            mode="markers",
+            marker_color=val["color"],
+            visible=True,
+        )
     )
-)
-
-# add bloom
-fig.add_trace(
-    go.Scattergeo(
-        name="BLOOM",
-        lon=lang_df[lang_df["bloom"] == True]["longitude"],
-        lat=lang_df[lang_df["bloom"] == True]["latitude"],
-        text=lang_df[lang_df["bloom"] == True]["Name"],
-        mode="markers",
-        marker_color="green",
-        visible=True,
-    )
-)
 
 fig.update_layout(
     autosize=True,
