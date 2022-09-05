d21bf91c9fd29f62f9b22fa1106e5eb40876751b
Sergei Averkiev <s.averkiev@kryptonite.ru>
2022-09-05 09:02:49 +0300

    Update generation script. Refactoring.

--- make_html.py
+++ make_html.py
@@ -11,28 +11,25 @@ pio.templates.default = "seaborn"
 lang_df = pd.read_csv("./langs.csv")
 
 fig = go.Figure()
-fig.update_geos(projection_type="natural earth")
-fig.update_geos(lataxis_showgrid=True, lonaxis_showgrid=True)
 fig.update_geos(
+    projection_type="natural earth",
     showcoastlines=True,
     coastlinecolor="#aaaaaa",
-)
-
-fig.update_layout(
-    font_color="#555555",
-    title_font_color="#444444",
-    title_text="Language models on the map",
-    title_x=0.5,
-    title_font_size=20,
-    legend_x=0.92,
+    lataxis_showgrid=True,
+    lonaxis_showgrid=True,
 )
 
 colors = px.colors.qualitative.Light24
 
 groups = {
-    "mT5": {"col": "mt5", "color": "blue"},
-    "mGPT": {"col": "mgpt", "color": "red"},
-    "BLOOM": {"col": "bloom", "color": "green"},
+    "mT5": {"col": "mt5", "color": "blue", "marker": "arrow-down", "marker-size": 9},
+    "mGPT": {"col": "mgpt", "color": "red", "marker": "arrow-up", "marker-size": 7},
+    "BLOOM": {
+        "col": "bloom",
+        "color": "green",
+        "marker": "arrow-left",
+        "marker-size": 5,
+    },
 }
 
 for name, val in groups.items():
@@ -44,16 +41,23 @@ for name, val in groups.items():
             text=lang_df[lang_df[val["col"]] == True]["Name"],
             mode="markers",
             marker_color=val["color"],
+            # marker_symbol=val["marker"],
             marker_symbol="diamond",
+            # marker_size=val["marker-size"],
             visible=True,
         )
     )
 
 fig.update_layout(
     autosize=True,
+    showlegend=True,
+    font_color="#555555",
+    title_font_color="#444444",
+    title_text="Language models on the map",
+    title_x=0.5,
+    title_font_size=20,
+    legend_x=0.92,
 )
-fig.update_layout(showlegend=True)
-
 
 fig.write_html("index.html", include_plotlyjs="cdn")
 
