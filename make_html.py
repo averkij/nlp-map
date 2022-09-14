e1cc0639466623aabcb382091c7d5b37c0b00fde
Sergei Averkiev <s.averkiev@kryptonite.ru>
2022-09-14 11:33:53 +0300

    Update scripts.

--- make_html.py
+++ make_html.py
@@ -1,6 +1,5 @@
 #%%
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
@@ -10,19 +9,52 @@ pio.templates.default = "seaborn"
 
 lang_df = pd.read_csv("./langs.csv")
 
-fig = go.Figure()
-fig.update_geos(
-    projection_type="natural earth",
-    showcoastlines=True,
-    coastlinecolor="#aaaaaa",
-    lataxis_showgrid=True,
-    lonaxis_showgrid=True,
-)
+BG_COLOR = "#659dbd"
 
-colors = px.colors.qualitative.Light24
+
+def create_figure():
+    fig = go.Figure()
+    fig.update_geos(
+        projection_type="natural earth",
+        showcoastlines=True,
+        coastlinecolor="#aaaaaa",
+        lataxis_showgrid=True,
+        lonaxis_showgrid=True,
+        bgcolor=BG_COLOR,
+        showocean=True,
+        oceancolor="white",
+    )
+    return fig
+
+
+def update_figure(fig, title):
+    fig.update_layout(
+        autosize=True,
+        showlegend=True,
+        font_color="#555555",
+        legend_bgcolor="white",
+        paper_bgcolor=BG_COLOR,
+        title=dict(
+            text=title,
+            x=0.5,
+            y=0.96,
+            font=dict(family="Josefin Sans", size=32, color="white"),
+        ),
+        legend=dict(
+            x=0.92,
+            y=1,
+            font=dict(family="Arial", size=12, color="black"),
+            bgcolor="white",
+            bordercolor="#555555",
+            borderwidth=1,
+        ),
+    )
+
+
+fig = create_figure()
 
 groups = {
-    "mT5": {"col": "mt5", "color": "blue", "marker": "arrow-down", "marker-size": 9},
+    "mT5": {"col": "mt5", "color": "blue", "marker": "arrow-down", "marker-size": 7},
     "mGPT": {"col": "mgpt", "color": "red", "marker": "arrow-up", "marker-size": 7},
     "BLOOM": {
         "col": "bloom",
@@ -30,9 +62,17 @@ groups = {
         "marker": "arrow-left",
         "marker-size": 5,
     },
+    "XGLM": {
+        "col": "xglm",
+        "color": "orange",
+        "marker": "arrow-right",
+        "marker-size": 7,
+    },
 }
 
-for name, val in groups.items():
+colors = px.colors.qualitative.Plotly
+
+for i, (name, val) in enumerate(groups.items()):
     fig.add_trace(
         go.Scattergeo(
             name=name,
@@ -40,27 +80,18 @@ for name, val in groups.items():
             lat=lang_df[lang_df[val["col"]] == True]["latitude"],
             text=lang_df[lang_df[val["col"]] == True]["Name"],
             mode="markers",
-            marker_color=val["color"],
-            # marker_symbol=val["marker"],
-            marker_symbol="diamond",
-            # marker_size=val["marker-size"],
+            marker=dict(
+                size=7,
+                color=colors[i % 10],
+                symbol=val["marker"],
+            ),
             visible=True,
         )
     )
 
-fig.update_layout(
-    autosize=True,
-    showlegend=True,
-    font_color="#555555",
-    title_font_color="#444444",
-    title_text="Language models on the map",
-    title_x=0.5,
-    title_font_size=20,
-    legend_x=0.92,
-)
+update_figure(fig, title="Language models on the map")
 
 fig.write_html("index.html", include_plotlyjs="cdn")
-
 fig.show()
 
 # %%
